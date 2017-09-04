754e921301296b7b158debe9880be1846a436915
Delphi Systems <contact@delphi.systems>
2017-09-04 13:40:08 -0400

    Use multiplication instead of division; require user calculation of 'amount'

--- RedemptionContract.sol
+++ RedemptionContract.sol
@@ -7,28 +7,34 @@ contract Token {
 }
 
 contract RedemptionContract {
-  address public token;         // the token address
-  uint public exchangeRate;     // number of tokens per ETH
+  address public token;         	    // the token address
+  uint public exchangeRateNumerator;	// exchangeRateNumerator / exchangeRateDenominator = ETH per token
+  uint public exchangeRateDenominator;	// exchangeRateNumerator / exchangeRateDenominator = ETH per token
+
+  // exchangeRateNumerator      = 41490101
+  // exchangeRateDenominator    = 100000000000
 
   event Redemption(address redeemer, uint tokensDeposited, uint redemptionAmount);
 
-  function RedemptionContract(address _token, uint _exchangeRate) {
-      token = _token;
-      exchangeRate = _exchangeRate;
+  function RedemptionContract(address _token, uint _exchangeRateNumerator, uint _exchangeRateDenominator) {
+    token = _token;
+    exchangeRateNumerator = _exchangeRateNumerator;
+    exchangeRateDenominator = _exchangeRateDenominator;
   }
 
   function fund() payable {}
 
   function redeemTokens(uint amount) {
     // NOTE: redeemTokens will only work once the sender has approved 
-    // the RedemptionContract address for the deposit amount 
-    require(Token(token).transferFrom(msg.sender, this, amount));
+    // the RedemptionContract address for (amount*exchangeRateDenominator)
+    uint tokenTransferAmount = exchangeRateDenominator * amount;
+    require(Token(token).transferFrom(msg.sender, this, tokenTransferAmount));
     
-    uint redemptionValue = amount / exchangeRate; 
+    uint redemptionValue = amount * exchangeRateNumerator;
     
     msg.sender.transfer(redemptionValue);
     
-    Redemption(msg.sender, amount, redemptionValue);
+    Redemption(msg.sender, tokenTransferAmount, redemptionValue);
   }
 
 }
